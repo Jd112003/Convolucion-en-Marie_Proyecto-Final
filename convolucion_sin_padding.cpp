0c94ebe36a2ad8fe4ba8b940da85ae93151e6d95
Jd112003 <150453862+Jd112003@users.noreply.github.com>
2024-11-25 13:36:27 -0500

    Update convolucion_sin_padding.cpp

--- convolucion_sin_padding.cpp
+++ convolucion_sin_padding.cpp
@@ -4,24 +4,26 @@
 using namespace std;
 
 int main() {
-    vector<vector<int>> imagen{{1, 2, 3, 4},
-                               {1, 2, 3, 4},
-                               {1, 2, 3, 4},
-                               {1, 2, 3, 4}};
+    vector<vector<int>> imagen{{1, 2, 3},
+                               {1, 2, 3},
+                               {1, 2, 3},
+                               {1, 2, 3}};
                                
     vector<vector<int>> kernel{{-5, -6, -7},
                                {-8, -9, -10},
                                {-11, -12, -13}};
                                
     int imageSize = imagen.size();
+    int rowImage = imagen[0].size();
     int kernelSize = kernel.size();
     int padding = kernelSize / 2;
 
-    vector<vector<int>> resultado(imageSize, vector<int>(imageSize, 0));
+
+    vector<vector<int>> resultado(imageSize, vector<int>(rowImage, 0));
 
     // Convolución con kernel dinámicamente reducido
     for (int i = 0; i < imageSize; i++) {
-        for (int j = 0; j < imageSize; j++) {
+        for (int j = 0; j < rowImage; j++) {
             int sum = 0;
 
             for (int ki = 0; ki < kernelSize; ki++) {
@@ -29,7 +31,7 @@ int main() {
                     int ni = i + ki - padding; // Índice de fila ajustado
                     int nj = j + kj - padding; // Índice de columna ajustado
 
-                    if (ni >= 0 && ni < imageSize && nj >= 0 && nj < imageSize) {
+                    if (ni >= 0 && ni < imageSize && nj >= 0 && nj < rowImage) {
                         sum += imagen[ni][nj] * kernel[ki][kj];
                     }
                 }
@@ -41,7 +43,7 @@ int main() {
 
     // Mostrar el resultado
     for (int i = 0; i < imageSize; i++) {
-        for (int j = 0; j < imageSize; j++) {
+        for (int j = 0; j < rowImage; j++) {
             cout << resultado[i][j] << " ";
         }
         cout << endl;
